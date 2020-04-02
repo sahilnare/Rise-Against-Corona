7b025499bc06dda3fbca5e857f9b26143ac88d3f
Sahil Nare <37807526+sahilnare@users.noreply.github.com>
2020-04-02 13:10:54 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,14 +2,7 @@
 
 
   const firebaseConfig = {
-      apiKey: "AIzaSyCpxRdOoDt5ZBqJURbjbBUmsxWL0W6mbrs",
-      authDomain: "dayslater-b738c.firebaseapp.com",
-      databaseURL: "https://dayslater-b738c.firebaseio.com",
-      projectId: "dayslater-b738c",
-      storageBucket: "dayslater-b738c.appspot.com",
-      messagingSenderId: "193128042568",
-      appId: "1:193128042568:web:81dc29b4b6fb01e200087d",
-      measurementId: "G-T59CE7GFYW"
+      //Firebase Configuration Code
     };
 
 
